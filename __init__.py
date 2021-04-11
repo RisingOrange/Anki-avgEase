d8de97690038083fd162b2397211ad62f4c39486
RisingOrange <jakub.fidler@protonmail.com>
2021-04-11 20:40:01 +0200

    Update for Anki 2.1.41

--- __init__.py
+++ __init__.py
@@ -1,27 +1,58 @@
 # Average Ease
-# Anki 2 addon
-# Author EJS
+# Anki 2.1 addon
+# Original Author EJS
 # https://eshapard.github.io/
 #
 # Sets the initial ease factor of a deck options group to the average
 # ease factor of the mature cards within that deck options group.
-from anki.hooks import addHook
-from aqt import mw
-#import time
+import argparse
 
-# Find decks in settings group
-def find_decks_in_settings_group(group_id):
-    members = []
-    decks = mw.col.decks.decks
-    for d in decks:
-        if 'conf' in decks[d] and int(decks[d]['conf']) == int(group_id):
-            members.append(d)
-    return members
+from anki.hooks import addHook
+
+COL = None
+
+
+def update_ease_factors():
+    for option_group in COL.decks.all_config():
+        print(option_group['id'], option_group['name'])
+        update_ease_factor(option_group)
+
+
+# Average mature card ease factor in settings group
+def update_ease_factor(option_group):
+    avg_ease, cur_ease = mature_ease_in_settings_group(option_group)
+    # print(f'{option_group["name"]}: {avg_ease} {cur_ease}')
+
+    option_group['new']['initialFactor'] = int(avg_ease)
+    COL.decks.update_config(option_group)
+    COL.decks.save(option_group)
+    COL.decks.flush()
+
+
+def mature_ease_in_settings_group(option_group):
+    tot_mature_cards = 0
+    weighted_ease = 0
+    avg_mature_ease = 0
+
+    decks = COL.decks.didsForConf(option_group)
+    for deck in decks:
+        mature_cards, mature_ease = average_ease_in_deck(deck)
+        tot_mature_cards += mature_cards
+        weighted_ease += mature_cards * mature_ease
+    if tot_mature_cards > 0 and weighted_ease:
+        avg_mature_ease = int(weighted_ease / tot_mature_cards)
+    else:
+        # not enough data; don't change the init ease factor
+        avg_mature_ease = option_group["new"]["initialFactor"]
+
+    cur_ease = option_group['new']['initialFactor']
+    return avg_mature_ease, cur_ease
+
 
 # Find average ease and number of mature cards in deck
 #   mature defined as having an interval > 90 day
-def find_average_ease_in_deck(deck_id):
-    mature_cards = mw.col.db.scalar("""select
+def average_ease_in_deck(deck_id):
+    mature_cards = COL.db.scalar("""select
         count()
         from cards where
         type = 2 and
@@ -29,7 +60,7 @@ def find_average_ease_in_deck(deck_id):
         did = ?""", deck_id)
     if not mature_cards:
         mature_cards = 0
-    mature_ease = mw.col.db.scalar("""select
+    mature_ease = COL.db.scalar("""select
         avg(factor)
         from cards where
         type = 2 and
@@ -40,59 +71,23 @@ def find_average_ease_in_deck(deck_id):
     return mature_cards, mature_ease
 
 
-# Average mature card ease factor in settings group
-def mature_ease_in_settings_group(dogID):
-    tot_mature_cards = 0
-    weighted_ease = 0
-    avg_mature_ease = 0
-    cur_ease = 0
-    group_id = dogID
-    if group_id:
-        # Find decks and cycle through
-        decks = find_decks_in_settings_group(group_id)
-        for d in decks:
-            mature_cards, mature_ease = find_average_ease_in_deck(d)
-            tot_mature_cards += mature_cards
-            weighted_ease += mature_cards * mature_ease
-        if tot_mature_cards > 0 and weighted_ease:
-            avg_mature_ease = int(weighted_ease / tot_mature_cards)
-        else:
-            # not enough data; don't change the init ease factor
-            avg_mature_ease = mw.col.decks.dconf[group_id]["new"]["initialFactor"]
-        cur_ease = mw.col.decks.dconf[group_id]["new"]["initialFactor"]
-    return avg_mature_ease, cur_ease
+def main():
+    global COL
+    COL = mw.col
+    update_ease_factors()
 
 
-# update initial ease factor of a settings group
-def update_initial_ease_factor(dogID, ease_factor):
-    group_id = dogID
-    if group_id:
-        if group_id in mw.col.decks.dconf:
-            mw.col.decks.dconf[group_id]["new"]["initialFactor"] = int(ease_factor)
-            mw.col.decks.save(mw.col.decks.dconf[group_id])
-            #mw.col.decks.flush()
-# main function
-def update_ease_factor(dogID):
-    avg_ease, cur_ease = mature_ease_in_settings_group(dogID)
-    #utils.showInfo("dogID: %s AvgEase: %s" % (dogID, avg_ease))
-    update_initial_ease_factor(dogID, avg_ease)
+try:
+    from aqt import mw
+    addHook("profileLoaded", main)
 
-# run this on profile load
-def update_ease_factors():
-    #find all deck option groups
-    dconf = mw.col.decks.dconf
-    #create progress bar
-    #ogs = len(dconf)
-    #mw.progress.start(max = ogs, label = "Init Ease Factor: %s" % ogs)
-    #cycle through them one by one
-    #i = 1
-    for k in dconf:
-        update_ease_factor(k)
-        #mw.progress.update("Init Ease Factor: %s" % dconf[k]['name'], i)
-        #i += 1
-        #time.sleep(1)
-    #mw.progress.finish()
-    mw.reset()
+except:
+    from anki import Collection as Col
+    parser = argparse.ArgumentParser()
+    parser.add_argument('collection_path')
+    args = parser.parse_args()
 
-# add hook to 'profileLoaded'
-addHook("profileLoaded", update_ease_factors)
+    COL = Col(args.collection_path)
+    update_ease_factors()
+    COL.save()
+    COL.flush()
